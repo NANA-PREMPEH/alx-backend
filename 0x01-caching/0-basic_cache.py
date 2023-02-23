0dd464674519eb4d51a1254586e18a0f223d0aa7
William Osei Prempeh <52207181+NANA-PREMPEH@users.noreply.github.com>
2023-02-23 05:12:35 +0000

    Update 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,23 +1,28 @@
-#!/usr/bin/env python3
-from base_caching import BaseCaching
-"""
-Basic dictionary
-"""
+#!/usr/bin/python3
+''' Basic Dictionary: Create a class BasicCache that inherits from BaseCaching
+                      and is a caching system
+'''
+
+BaseCaching = __import__('base_caching').BaseCaching
+
 
 class BasicCache(BaseCaching):
-    '''Caching system inheriting from
-       BaseCaching'''
+    ''' A basic cache.
+        Inherits from class BaseCaching.
+        Attributes:
+          put - method that adds a key/value pair to cache
+          get - method that retrieves a key/value pair from cache
+    '''
 
-    def put(self,key, item):
-        '''Assign to the dictionary'''
-        if key is None or item is None:
-            pass
-        else:
+    def put(self, key, item):
+        ''' Add key/value pair to cache.
+        If either `key` or `item` is None, do nothing. '''
+        if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        '''Get an item by key'''
-        if key is None or self.cache_data.get(key) is None:
-            return None
-        else:
+        ''' Return value stored in `key` of cache.
+        If key is None or does not exist in cache, return None. '''
+        if key is not None and key in self.cache_data:
             return self.cache_data[key]
+        return None
