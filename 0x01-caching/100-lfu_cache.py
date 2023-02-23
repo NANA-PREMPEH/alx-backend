0a4cdc54e2e60ba893ca72a97ce3c7d8b99e8e8f
William Osei Prempeh <52207181+NANA-PREMPEH@users.noreply.github.com>
2023-02-23 05:10:12 +0000

    Update 100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,40 +1,65 @@
 #!/usr/bin/python3
-""" LFUCache module
-"""
-from base_caching import BaseCaching
+''' LFU Caching: Create a class LFUCache that inherits from BaseCaching
+                 and is a caching system '''
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """ BasicCache defines:
-      - Inherits from BaseCaching
-      - Cashing System with no limit
-      - where your data are stored (in a dictionary)
-    """
+    ''' An LFU cache.
+        Inherits all behaviors from BaseCaching except, upon any attempt to add
+        an entry to the cache when it is at max capacity (as specified by
+        BaseCaching.MAX_ITEMS), it discards the least frequently used entry to
+        accommodate for the new one.
+        Attributes:
+          __init__ - method that initializes class instance
+          put - method that adds a key/value pair to cache
+          get - method that retrieves a key/value pair from cache '''
+
     def __init__(self):
-        """ BaseCaching """
-        self.cache = []
+        ''' Initialize class instance. '''
         super().__init__()
+        self.keys = []
+        self.uses = {}
 
     def put(self, key, item):
-        """ Assign to the dictionary """
-        if key and item:
-            if key in self.cache_data:
-                self.cache.remove(key)
+        ''' Add key/value pair to cache data.
+            If cache is at max capacity (specified by BaseCaching.MAX_ITEMS),
+            discard least frequently used entry to accommodate new entry. '''
+
+        if key is not None and item is not None:
+            if (len(self.keys) == BaseCaching.MAX_ITEMS and
+                    key not in self.keys):
+                discard = self.keys.pop(self.keys.index(self.findLFU()))
+                del self.cache_data[discard]
+                del self.uses[discard]
+                print('DISCARD: {:s}'.format(discard))
             self.cache_data[key] = item
-            self.cache.append(key)
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            discard = self.cache.pop(0)
-            del self.cache_data[discard]
-            print('DISCARD:', discard)
-        else:
-            pass
+            if key not in self.keys:
+                self.keys.append(key)
+                self.uses[key] = 0
+            else:
+                self.keys.append(self.keys.pop(self.keys.index(key)))
+                self.uses[key] += 1
 
     def get(self, key):
-        """ Get an item by key
-        """
-        if key is None or self.cache_data.get(key) is None:
-            return None
-        else:
-            self.cache.remove(key)
-            self.cache.append(key)
+        ''' Return value stored in `key` key of cache.
+            If key is None or does not exist in cache, return None. '''
+        if key is not None and key in self.cache_data:
+            self.keys.append(self.keys.pop(self.keys.index(key)))
+            self.uses[key] += 1
             return self.cache_data[key]
+        return None
+
+    def findLFU(self):
+        ''' Return key of least frequently used item in cache.
+            If multiple items have the same amount of uses, return the least
+            recently used one. '''
+        items = list(self.uses.items())
+        freqs = [item[1] for item in items]
+        least = min(freqs)
+
+        lfus = [item[0] for item in items if item[1] == least]
+        for key in self.keys:
+            if key in lfus:
+                return key
